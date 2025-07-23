19cbe1e91eeed13891d7f13d5f1356f994b69f4c
lara <larataqi12@gmail.com>
2025-07-24 01:37:57 +0300

    create pyramid shape with star using for loop

--- pyramid.js
+++ pyramid.js
@@ -1,1 +1,20 @@
-alert('its me');
+let i=0;
+let j=0;
+let x=0;
+let y=0;
+for (i=0;i<7;i++){
+  let star =" ";
+  for (j=0;j<i;j++)
+  {
+   star +="* ";
+  }
+  console.log(star);
+}
+for (x=0;x<7;x++){
+  let stars =" ";
+  for (y=x;y<7;y++)
+  {
+   stars +="* ";
+  }
+  console.log(stars);
+}
